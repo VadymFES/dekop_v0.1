20e30432a6ba8b08d107f9c443abf1c8ceeaed24
Vadym Fesiuk <vadymfesiuk@gmail.com>
2024-10-27 19:05:08 +0100

    add label on image

--- src/app/components/productCard/productCard.tsx
+++ src/app/components/productCard/productCard.tsx
@@ -11,16 +11,115 @@ interface ProductCardProps {
     type: string;
     rating: number;
     price: number;
+    isBestseller?: boolean; // New prop to indicate bestseller status
+    isNew?: boolean; // New prop to indicate new product status
   };
 }
 
+const products = [
+  {
+    id: 1,
+    image: "https://images.unsplash.com/photo-1728664550581-e5fa836e720a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Беатріс",
+    price: 12000,
+    rating: 4,
+    type: "Кутові дивани",
+    isBestseller: true,
+  },
+  {
+    id: 2,
+    image: "https://images.unsplash.com/photo-1728664550581-e5fa836e720a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Ріо 2",
+    price: 200,
+    rating: 3,
+    type: "Дивани",
+    isBestseller: true,
+  },
+  {
+    id: 3,
+    image: "https://images.unsplash.com/photo-1728664550581-e5fa836e720a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product 3",
+    price: 300,
+    rating: 2.3,
+    type: "sofa",
+  },
+  {
+    id: 4,
+    image: "https://images.unsplash.com/photo-1728664550581-e5fa836e720a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product 4",
+    price: 400,
+    rating: 4.5,
+    type: "wardrobe",
+  },
+  {
+    id: 5,
+    image: "https://images.unsplash.com/photo-1728664550581-e5fa836e720a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product 5",
+    price: 500,
+    rating: 3.7,
+    type: "sofa",
+    isBestseller: true,
+  },
+  {
+    id: 6,
+    image: "https://images.unsplash.com/photo-1728664550581-e5fa836e720a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product 6",
+    price: 600,
+    rating: 4.2,
+    type: "kitchen",
+    isBestseller: true,
+  },
+  {
+    id: 7,
+    image: "https://images.unsplash.com/photo-1728664550581-e5fa836e720a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product 6",
+    price: 600,
+    rating: 4.2,
+    type: "kitchen",
+  },
+  {
+    id: 8,
+    image: "https://images.unsplash.com/photo-1728664550581-e5fa836e720a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product 8",
+    price: 600,
+    rating: 4.2,
+    type: "kitchen",
+  },
+  {
+    id: 9,
+    image: "https://images.unsplash.com/photo-1728664550581-e5fa836e720a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product 9",
+    price: 600,
+    rating: 4.2,
+    type: "kitchen",
+  },
+  {
+    id: 10,
+    image: "https://images.unsplash.com/photo-1728664550581-e5fa836e720a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product 10",
+    price: 600,
+    rating: 4.2,
+    type: "kitchen",
+  },
+  {
+    id: 11,
+    image: "https://images.unsplash.com/photo-1728664550581-e5fa836e720a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Product 11",
+    price: 600,
+    rating: 4.2,
+    type: "kitchen",
+  },
+];
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
     <div className={styles.productCard}>
       <Link href={`/product/${product.id}`}>
         <div>
-          <img src={product.image} alt={product.name} className={styles.productImage} />
+          <div className={styles.imageWrapper}>
+            <img src={product.image} alt={product.name} className={styles.productImage} />
+            {product.isBestseller && <div className={styles.salesLeaderLabel}>Sales Leader</div>}
+          </div>
 
           <div className={styles.productDetails}>
             <div className={styles.productInfo}>
@@ -39,7 +138,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       <button
         className={styles.addToCartButton}
         onClick={() => {
-          //"add to cart" logic here
+          // "add to cart" logic here
           console.log('Product added to cart');
         }}
       >
