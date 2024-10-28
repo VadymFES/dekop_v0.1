ed41335e5c1111301823f7f9afd65657fee165dd
Vadym Fesiuk <vadymfesiuk@gmail.com>
2024-10-28 20:02:33 +0100

    add blure on open menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
 "use client";
 
+import { useState } from "react";
 import Header from "../app/components/header/header.tsx";
 import Main from "../app/components/main/main.tsx";
 import Footer from "./components/footer/footer.tsx";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className={styles.page}>
-      <Header />
-      <Main />
-      <Footer />
+      {menuOpen && <div className={styles.overlay} onClick={handleMenuToggle}></div>}
+
+      <Header menuOpen={menuOpen} onMenuToggle={handleMenuToggle} />
+
+      <div className={`${styles.content} ${menuOpen ? styles.blurred : ''}`}>
+        <Main />
+        <Footer />
+      </div>
     </div>
   );
 }
