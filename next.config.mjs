fe0e6dbf99cf3008135e4966c4c164a32addab50
Vadym Fesiuk <vadymfesiuk@gmail.com>
2025-01-15 21:46:47 +0100

    Configure Next.js settings: enable React strict mode and allow images from specific remote domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                // This pattern will enable the Image component
+                // to load images from the domain `example.com`
+                // and `www.example.com`
+                protocol: 'https',
+                hostname: 'fullhouse.uz'
+            },
+        ],
+    },
+};
+
+
 
 export default nextConfig;
