afd0ad4b6cf17c81ba8b0cc7f012d7ce5d6ca913
Vadym Fesiuk <vadymfesiuk@gmail.com>
2025-01-15 21:45:45 +0100

    Update materialsMain styles and component: enhance responsive design, adjust layout, and improve image handling

--- app/components/materialsMain/materialsMain.tsx
+++ app/components/materialsMain/materialsMain.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import styles from "./materialsMain.module.css";
+import Image from "next/image";
 
 const materials = [
-    { id: 1, image: "https://fullhouse.uz/d/vidy_ldsp.jpg", type: "ДСП", description: "Renowned for its unmatched durability and resilience, this premium material excels in challenging environments. Its robust construction ensures peak performance in high-stress scenarios, making it ideal for demanding applications. Resistant to wear, impact, and harsh conditions, it retains its quality and integrity over time. Its exceptional reliability makes it the preferred choice for projects requiring uncompromising standards of performance and durability" },
-    { id: 2, image: "https://fullhouse.uz/d/vidy_ldsp.jpg", type: "ЛДСП", description: "A top-tier material celebrated for its exceptional strength and toughness, designed to thrive under extreme conditions. Its superior craftsmanship ensures reliable performance in critical applications. With excellent resistance to impact, abrasion, and wear, it delivers lasting quality and functionality. Chosen for its dependability, it’s the go-to solution for projects demanding premium performance and long-term durability" },
+    { id: 1, image: "https://fullhouse.uz/d/vidy_ldsp.jpg", type: "ДСП", description: "This high-performance material is praised for its unparalleled durability and robust design, perfectly suited for the most demanding environments. Built to endure high-pressure situations, it resists impact, abrasion, and other forms of damage. Its superior reliability and quality make it a trusted choice for applications requiring exceptional strength and long-lasting performance " },
+    { id: 2, image: "https://fullhouse.uz/d/vidy_ldsp.jpg", type: "ЛДСП", description: "This high-performance material is praised for its unparalleled durability and robust design, perfectly suited for the most demanding environments. Built to endure high-pressure situations, it resists impact, abrasion, and other forms of damage. Its superior reliability and quality make it a trusted choice for applications requiring exceptional strength and long-lasting performance" },
     { id: 3, image: "https://fullhouse.uz/d/vidy_ldsp.jpg", type: "МДФ", description: "This high-performance material is praised for its unparalleled durability and robust design, perfectly suited for the most demanding environments. Built to endure high-pressure situations, it resists impact, abrasion, and other forms of damage. Its superior reliability and quality make it a trusted choice for applications requiring exceptional strength and long-lasting performance" },
     { id: 4, image: "https://fullhouse.uz/d/vidy_ldsp.jpg", type: "ЕКОШКІРА", description: "Known for its exceptional strength and endurance, this premium material is engineered for extreme conditions and high-stress applications. Its superior resilience ensures lasting integrity, even in challenging environments. Impact and abrasion-resistant, it delivers consistent performance, making it the ideal choice for projects where reliability and durability are paramount" },
     { id: 5, image: "https://fullhouse.uz/d/vidy_ldsp.jpg", type: "ВЕЛЮР", description: "A premium-grade material famed for its exceptional toughness and reliability, offering unparalleled performance in demanding conditions. Designed to withstand impact, abrasion, and heavy wear, it maintains its structural integrity over time. Its superior quality ensures it is the preferred choice for critical applications requiring maximum strength and durability" },
-    { id: 6, image: "https://fullhouse.uz/d/vidy_ldsp.jpg", type: "ТЕКСТИЛЬ", description: "" },
+    { id: 6, image: "https://fullhouse.uz/d/vidy_ldsp.jpg", type: "ТЕКСТИЛЬ", description: "A premium-grade material famed for its exceptional toughness and reliability, offering unparalleled performance in demanding conditions. Designed to withstand impact, abrasion, and heavy wear, it maintains its structural integrity over time. Its superior quality ensures it is the preferred choice for critical applications requiring maximum strength and durability" },
 ];
 
 const MaterialsMain = () => {
@@ -42,7 +43,13 @@ const MaterialsMain = () => {
                         <p className={styles.materialDescription}>{filteredMaterial.description}</p>
                     </div>
                     <div className={styles.materialImageWrapper}>
-                        <img src={filteredMaterial.image} alt={filteredMaterial.type} className={styles.materialImage} />
+                        <Image
+                        src={filteredMaterial.image} 
+                        alt={filteredMaterial.type}
+                        className={styles.materialImage}
+                        width={300}
+                        height={300}
+                            />
                     </div>
                 </div>
             )}
