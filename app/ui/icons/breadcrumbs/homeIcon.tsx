80de7b27224817ba13381eedea14cfdb7ed2df72
Vadym Fesiuk <vadymfesiuk@gmail.com>
2025-02-01 20:44:43 +0100

    feat: add HomeIcon component for breadcrumb navigation

---
+++ app/ui/icons/breadcrumbs/homeIcon.tsx
@@ -0,0 +1,12 @@
+export const HomeIcon = () => (
+
+<svg width="1.2em" height="1.2em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M20.25 21.75H3.75V12.75H2.42625C1.57275 12.75 1.1745 11.6857 1.81725 11.124L10.5982 2.80875C11.3842 2.064 12.615 2.064 13.401 2.80875L22.1812 11.124C22.8247 11.685 22.4265 12.75 21.5722 12.75H20.25V21.75Z"
+        stroke="#160101" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+    <path
+        d="M15 21.75H9V17.25C9 15.5932 10.3432 14.25 12 14.25C13.6568 14.25 15 15.5932 15 17.25V21.75Z"
+        stroke="#160101" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+</svg>
+
+);
