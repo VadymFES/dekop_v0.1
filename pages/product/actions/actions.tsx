381d2778a6b147aa12a419622f850a1af28345db
Vadym Fesiuk <vadymfesiuk@gmail.com>
2025-02-10 21:47:52 +0100

    feat: enhance product action styles for improved responsiveness and layout

--- pages/product/actions/actions.tsx
+++ pages/product/actions/actions.tsx
@@ -7,11 +7,10 @@ import { AlertIcon } from '@/app/ui/icons/alert/alertIcon';
 import Image from 'next/image';
 
 interface ProductActionsProps {
-  product?: ProductWithImages; // Mark as optional if it might be undefined
+  product?: ProductWithImages;
 }
 
 const ProductActions = ({ product }: ProductActionsProps) => {
-  // Hooks are called unconditionally at the top:
   const [quantity, setQuantity] = useState(1);
   const [selectedColor, setSelectedColor] = useState(
     product?.colors?.[0] || { color: 'No Color', image_url: '' }
@@ -42,143 +41,141 @@ const ProductActions = ({ product }: ProductActionsProps) => {
 
   return (
     <>
-      <div className={styles.stickyWrapper}>
-        <div className={styles.topSection}>
-          <h1 className={styles.productName}>{name}</h1>
-          <div className={styles.availabilityWrapper}>
-            <span className={stock > 0 ? styles.inStock : styles.outOfStock}>
-              {stock > 0 ? 'Є в наявності' : 'Немає в наявності'}
-            </span>
-          </div>
-          <div className={styles.ratingWrapper}>
-            <span className={styles.productRating}>{rating}</span>
-            <span className={styles.productPrice}>{price} грн</span>
-          </div>
+    <div className={styles.stickyWrapper}>
+      <div className={styles.topSection}>
+        <h1 className={styles.productName}>{name}</h1>
+        <div className={styles.availabilityPriceWrapper}>
+          <span className={stock > 0 ? styles.inStock : styles.outOfStock}>
+            {stock > 0 ? 'Є в наявності' : 'Немає в наявності'}
+          </span>
+          <span className={styles.productRating}>{rating}</span>
+          <span className={styles.productPrice}>{price} грн</span>
+        </div>
+      </div>
+    </div>
+
+    <div className={styles.staticContainer}>
+      <div className={styles.dimensionsContainer}>
+        <div className={styles.specsItem}>
+          <span className={styles.specsTitle}>Довжина:</span>
+          <span>{specs?.dimensions.length}мм</span>
+        </div>
+        <div className={styles.specsItem}>
+          <span className={styles.specsTitle}>Ширина:</span>
+          <span>{specs?.dimensions.depth}мм</span>
+        </div>
+        <div className={styles.specsItem}>
+          <span className={styles.specsTitle}>Висота:</span>
+          <span>{specs?.dimensions.height}мм</span>
         </div>
       </div>
 
-      {/* Static Product Details */}
-      <div className={styles.staticContainer}>
-        <div className={styles.dimensionsContainer}>
-          <div className={styles.specsItem}>
-            <span className={styles.specsTitle}>Довжина:</span>
-            <span>{specs?.dimensions.length}мм</span>
-          </div>
-          <div className={styles.specsItem}>
-            <span className={styles.specsTitle}>Ширина:</span>
-            <span>{specs?.dimensions.depth}мм</span>
-          </div>
-          <div className={styles.specsItem}>
-            <span className={styles.specsTitle}>Висота:</span>
-            <span>{specs?.dimensions.height}мм</span>
-          </div>
-        </div>
-
-        {/* Custom Color Selection */}
-        {colors.length > 0 && (
-          <div className={styles.specsItem}>
-            <span className={styles.specsTitle}>Колір:</span>
-            <div className={styles.customDropdown} ref={dropdownRef}>
-              <div className={styles.selectedOption} onClick={() => setIsDropdownOpen((prev) => !prev)}>
-                {selectedColor?.image_url ? (
-                  <Image
-                    src={selectedColor.image_url}
-                    alt={selectedColor.color}
-                    className={styles.colorImage}
-                    width={30}
-                    height={30}
-                  />
-                ) : (
-                  <span style={{ fontSize: '11px' }}>No images</span>
-                )}
-                <span>{selectedColor.color}</span>
-                <DropdownArrow />
-              </div>
-
-              {isDropdownOpen && (
-                <div className={styles.dropdownOptions}>
-                  {colors.map((colorOption, index) => (
-                    <div
-                      key={index}
-                      className={styles.optionItem}
-                      onClick={() => {
-                        setSelectedColor(colorOption);
-                        setIsDropdownOpen(false);
-                      }}
-                    >
-                      {colorOption.image_url ? (
-                        <Image
-                          src={colorOption.image_url}
-                          alt={colorOption.color}
-                          className={styles.optionImage}
-                          width={30}
-                          height={30}
-                        />
-                      ) : (
-                        <span style={{ fontSize: '11px' }}>No images</span>
-                      )}
-                      <span>{colorOption.color}</span>
-                    </div>
-                  ))}
-                </div>
+      {colors.length > 0 && (
+        <div className={styles.specsItemDropdown}>
+          <span className={styles.specsTitle}>Колір:</span>
+          <div className={styles.customDropdown} ref={dropdownRef}>
+            <div
+              className={styles.selectedOption}
+              onClick={() => setIsDropdownOpen((prev) => !prev)}
+            >
+              {selectedColor?.image_url ? (
+                <Image
+                  src={selectedColor.image_url}
+                  alt={selectedColor.color}
+                  className={styles.colorImage}
+                  width={30}
+                  height={30}
+                />
+              ) : (
+                <span style={{ fontSize: '11px' }}>No images</span>
               )}
+              <span>{selectedColor.color}</span>
+              <DropdownArrow />
             </div>
-          </div>
-        )}
 
-        <div className={styles.disclaimerSection}>
-          <p className={styles.warning}>
-            <span className={styles.icon}>
-              <AlertIcon />
-            </span>
-            Відображення кольору на фотографії залежить від налаштувань вашого екрану і може відрізнятися від
-            реального кольору товару
-          </p>
-          <p className={styles.returnPolicy}>
-            <span className={styles.icon}>
-              <AlertIcon />
-            </span>
-            Гарантія та повернення
-          </p>
+            {isDropdownOpen && (
+              <div className={styles.dropdownOptions}>
+                {colors.map((colorOption, index) => (
+                  <div
+                    key={index}
+                    className={styles.optionItem}
+                    onClick={() => {
+                      setSelectedColor(colorOption);
+                      setIsDropdownOpen(false);
+                    }}
+                  >
+                    {colorOption.image_url ? (
+                      <Image
+                        src={colorOption.image_url}
+                        alt={colorOption.color}
+                        className={styles.optionImage}
+                        width={30}
+                        height={30}
+                      />
+                    ) : (
+                      <span style={{ fontSize: '11px' }}>No images</span>
+                    )}
+                    <span>{colorOption.color}</span>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
+      )}
+
+      <div className={styles.disclaimerSection}>
+        <p className={styles.warning}>
+          <span className={styles.icon}>
+            <AlertIcon />
+          </span>
+          Відображення кольору на фотографії залежить від налаштувань вашого екрану і може відрізнятися від
+          реального кольору товару
+        </p>
+        <p className={styles.returnPolicy}>
+          <span className={styles.icon}>
+            <AlertIcon />
+          </span>
+          Гарантія та повернення
+        </p>
       </div>
+    </div>
 
-      {/* Bottom Section */}
-      <div className={styles.stickyWrapperbtm}>
-        <div className={styles.bottomSection}>
-          <div className={styles.quantityControl}>
-            <button
-              onClick={() => setQuantity((prev) => Math.max(1, prev - 1))}
-              className={styles.quantityButton}
-            >
-              -
-            </button>
-            <input
-              type="number"
-              min="1"
-              value={quantity}
-              onChange={(e) => setQuantity(Math.max(1, parseInt(e.target.value, 10)))}
-              className={styles.quantityInput}
-            />
-            <button
-              onClick={() => setQuantity((prev) => prev + 1)}
-              className={styles.quantityButton}
-            >
-              +
-            </button>
-          </div>
-
+    <div className={styles.stickyWrapperbtm}>
+      <div className={styles.bottomSection}>
+        <div className={styles.quantityControl}>
           <button
-            className={styles.addToCartButton}
-            onClick={() => console.log('Added to cart')}
-            disabled={stock < 1}
+            onClick={() => setQuantity((prev) => Math.max(1, prev - 1))}
+            className={styles.quantityButton}
           >
-            ДОДАТИ В КОШИК <AddToCartIcon />
+            -
+          </button>
+          <input
+            type="number"
+            min="1"
+            value={quantity}
+            onChange={(e) => setQuantity(Math.max(1, parseInt(e.target.value, 10)))}
+            className={styles.quantityInput}
+          />
+          <button
+            onClick={() => setQuantity((prev) => prev + 1)}
+            className={styles.quantityButton}
+          >
+            +
           </button>
         </div>
+
+        <button
+          className={styles.addToCartButton}
+          onClick={() => console.log('Added to cart')}
+          disabled={stock < 1}
+        >
+          ДОДАТИ В КОШИК <AddToCartIcon />
+        </button>
       </div>
-    </>
-  );
+    </div>
+  </>
+);
 };
 
 export default ProductActions;
