2c991f356c9491bff3bfe065b47eefd022aa5286
Vadym Fesiuk <vadymfesiuk@gmail.com>
2025-02-16 21:27:57 +0100

    feat: wrap main component with PagesLayout for improved structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
-// pages/_app.tsx
-
 import QueryProvider from "@/app/providers/QueryProvider";
 import type { AppProps } from "next/app";
+import PagesLayout from "./layout";
+import "@/app/globals.css";
 
 export default function Pages({ Component, pageProps }: AppProps) {
   return (
     <QueryProvider>
-      <Component {...pageProps} />
+      <PagesLayout>
+        <Component {...pageProps} />
+      </PagesLayout>
     </QueryProvider>
   );
 }
