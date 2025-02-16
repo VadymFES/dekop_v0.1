e7a7941b893973b11a4a88087901ee99e28980e2
Vadym Fesiuk <vadymfesiuk@gmail.com>
2025-02-16 21:28:17 +0100

    feat: add Contact Us page with introductory message

---
+++ pages/contact-us/index.tsx
@@ -0,0 +1,8 @@
+export default function ContactUsPage() {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <p>We would love to hear from you. Please fill out the form below.</p>
+        </div>
+    );
+}
