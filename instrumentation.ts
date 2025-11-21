d3a2249cb9b1209611e61021b67a866b93c575ed
VadymFES <vadymfesiuk@gmail.com>
2025-11-21 21:16:12 +0100

    feature: add sentry for logging

--- instrumentation.ts
+++ instrumentation.ts
@@ -4,13 +4,17 @@
  * This file is automatically loaded by Next.js on server startup
  */
 
+
+import * as Sentry from '@sentry/nextjs';
+
 export async function register() {
-  // Only register Sentry in server environments
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("./sentry.server.config");
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('./sentry.server.config');
   }
 
-  if (process.env.NEXT_RUNTIME === "edge") {
-    await import("./sentry.edge.config");
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    await import('./sentry.edge.config');
   }
 }
+
+export const onRequestError = Sentry.captureRequestError;
